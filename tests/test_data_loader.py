875910d8b60b7d430eacd2c104cdf9ec297955fb
Jongkuk Lim <limjk@jmarple.ai>
2021-09-24 14:23:04 +0900

    Add dataset loader and test images.

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -4,12 +4,65 @@
 - Contact: limjk@jmarple.ai
 """
 
-from scripts.data_loader.data_loader import LoadImagesAndLabels
+import cv2
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from scripts.data_loader.data_loader import LoadImages, LoadImagesAndLabels
 
 
-def test_load_images_and_labels():
-    dataset_loader = LoadImagesAndLabels("../datasets/VOC/images/train2012")
+def test_load_images(show_gui: bool = False):
+    batch_size = 16
+    dataset = LoadImages(
+        "tests/res/datasets/VOC/images/train",
+        cache_images=None,
+        n_skip=0,
+        batch_size=batch_size,
+    )
 
+    dataset_loader = DataLoader(
+        dataset, batch_size=batch_size, collate_fn=LoadImages.collate_fn
+    )
+
+    pbar = tqdm(dataset_loader, desc="Load image test.")
+    n_run = 0
+    for (img, path, shapes) in pbar:
+        n_run += 1
+        for i in range(img.shape[0]):
+            np_image = img[i].numpy()[::-1].transpose((1, 2, 0))
+
+            if show_gui:
+                cv2.imshow("test", np_image)
+                cv2.waitKey(1)
+
+    assert n_run == 7
+
+def test_load_images_and_labels(show_gui: bool = False):
+    batch_size = 16
+    dataset = LoadImagesAndLabels(
+        "tests/res/datasets/VOC/images/train",
+        cache_images=None,
+        n_skip=0,
+        batch_size=batch_size,
+    )
+
+    dataset_loader = DataLoader(
+        dataset, batch_size=batch_size, collate_fn=LoadImagesAndLabels.collate_fn
+    )
+
+    pbar = tqdm(dataset_loader, desc="Load image test.")
+    n_run = 0
+    for (img, labels, path, shapes) in pbar:
+        n_run += 1
+        for i in range(img.shape[0]):
+            np_image = img[i].numpy()[::-1].transpose((1, 2, 0))
+
+            if show_gui:
+                cv2.imshow("test", np_image)
+                cv2.waitKey(1)
+
+    assert n_run == 7
 
 if __name__ == "__main__":
-    test_load_images_and_labels()
+    # test_load_images_and_labels()
+    test_load_images()

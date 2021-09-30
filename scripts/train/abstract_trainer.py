0f1e2fa70c7961beb0489ee9587f176c06ac1811
Haneol Kim <hekim@jmarple.ai>
2021-09-30 17:39:47 +0900

    Add abstract trainer

--- scripts/train/abstract_trainer.py
+++ scripts/train/abstract_trainer.py
@@ -3,74 +3,51 @@
 - Author: Haneol Kim
 - Contact: hekim@jmarple.ai
 """
-from abc import abstractmethod
-from typing import Any, Dict, Tuple
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List, Union
 
-import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 
 
-class AbstractTrainer(pl.LightningModule):
-    """Abstract traniner class."""
+class AbstractTrainer(ABC):
+    """Abstract trainer class."""
 
     def __init__(
-        self, model: nn.Module, hyp: Dict[str, Any], device: torch.device
+        self, model: nn.Module, hyp: Dict[str, Any], epochs: int, device: torch.device
     ) -> None:
-        """Initialize AbstractTrainer class.
-
-        Args:
-            model: torch model to train.
-            hyp: hyper parameter config.
-        """
+        """Initialize AbstractTrainer class."""
         super().__init__()
         self.model = model
         self.hyp = hyp
+        self.epochs = epochs
+        self.device = device
 
     @abstractmethod
-    def forward(self, x: torch.Tensor, *args: Any, **kwargs: Any) -> torch.Tensor:  # type: ignore
-        """Feed forward the model."""
-        pass
-
-    @abstractmethod
-    def configure_optimizers(self) -> torch.optim.Optimizer:
-        """Configure optimizers and return optimizer."""
-        pass
-
-    @abstractmethod
-    def training_step(  # type: ignore
-        self,
-        train_batch: Tuple[torch.Tensor, torch.Tensor],
-        batch_idx: int,
-        *args: Any,
-        **kwargs: Any
+    def training_step(
+        self, batch: Union[List[torch.Tensor], torch.Tensor], batch_idx: int
     ) -> torch.Tensor:
-        """Train a step (a batch).
+        """Train a step.
 
         Args:
-            train_batch: train batch in tuple (input_x, y_true).
-            batch_idx: current batch index.
-
-        Returns:
-            Result of loss function.
+            batch: batch data.
+            batch_idx: batch index.
         """
         pass
 
     @abstractmethod
-    def validation_step(  # type: ignore
-        self, val_batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    def validation_step(
+        self, batch: Union[List[torch.Tensor], torch.Tensor], batch_idx: int
     ) -> torch.Tensor:
-        """Validate a step (a batch).
+        """Validate a step.
 
         Args:
-            val_batch: validation data batch in tuple (input_x, true_y).
-            batch_idx: current batch index.
-
-        Returns:
-            Result of loss function.
+            batch: batch data.
+            batch_idx: batch index.
         """
         pass
 
-    # def load_pretrained_model(self, state_dict_dir: str) -> None:
-    #     """Load pretrained model from .pt file."""
-    #     self.model.load_state_dict(torch.load(state_dict_dir))
+    @abstractmethod
+    def init_optimizer(self) -> Any:
+        """Initialize optimizer."""
+        pass

3abea9ce1eeffdf4890ac3404e0ad80e46736d37
Jongkuk Lim <limjk@jmarple.ai>
2021-10-07 19:26:25 +0900

    Remove mosaic_prob argument

--- tests/test_train_model_builder.py
+++ tests/test_train_model_builder.py
@@ -43,7 +43,6 @@ def test_train_model_builder() -> None:
         preprocess=lambda x: (x / 255.0).astype(np.float32),
         rect=False,
         pad=0,
-        mosaic_prob=cfg["hyper_params"]["mosaic"],
     )
     train_loader = DataLoader(
         train_dataset,

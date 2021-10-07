73cc4dfb1b6995612df65f858bf963015b7a11c4
Jongkuk Lim <limjk@jmarple.ai>
2021-10-07 19:20:28 +0900

    Add segmentation label type. Add yolo augmentation.

--- tests/test_augmentation.py
+++ tests/test_augmentation.py
@@ -4,6 +4,7 @@
 - Contact: limjk@jmarple.ai
 """
 
+import math
 import os
 
 import cv2
@@ -22,11 +23,12 @@ from scripts.utils.plot_utils import draw_labels
 def test_multi_aug_policies(show_gui: bool = False):
     label2str = LABELS["COCO"]
     batch_size = 16
+    minimum_pixel = 4
 
-    with open(os.path.join("tests", "res", "configs", "augmentation.yaml")) as f:
-        aug_cfg = yaml.safe_load(f)
+    with open(os.path.join("tests", "res", "configs", "train_config_sample.yaml")) as f:
+        cfg = yaml.safe_load(f)
 
-    aug_policy = MultiAugmentationPolicies(aug_cfg["augmentation"])
+    aug_policy = MultiAugmentationPolicies(cfg["augmentation"])
 
     dataset = LoadImagesAndLabels(
         "tests/res/datasets/coco/images/val2017",
@@ -35,7 +37,8 @@ def test_multi_aug_policies(show_gui: bool = False):
         batch_size=batch_size,
         rect=False,
         augmentation=aug_policy,
-        mosaic_prob=0.5,
+        yolo_augmentation=cfg["yolo_augmentation"],
+        label_type="segments",
     )
     dataset_loader = DataLoader(
         dataset, batch_size=batch_size, collate_fn=LoadImagesAndLabels.collate_fn
@@ -45,15 +48,33 @@ def test_multi_aug_policies(show_gui: bool = False):
     n_run = 0
     for (img, labels, path, shapes) in pbar:
         n_run += 1
+        minimum_box_size = minimum_pixel / math.sqrt(img.shape[2] * img.shape[3])
+
         for i in range(img.shape[0]):
             np_image = img[i].numpy()[::-1].transpose((1, 2, 0))
             label_list = labels[labels[:, 0] == i][:, 1:]
+
+            smaller_width_than_minimum_pixel = label_list[:, 3] < minimum_box_size
+            smaller_height_than_minimum_pixel = label_list[:, 4] < minimum_box_size
+
+            smaller_box_idx = torch.logical_or(
+                smaller_width_than_minimum_pixel, smaller_height_than_minimum_pixel
+            )
+
+            # label_list = label_list[torch.logical_not(smaller_box_idx)]
+            # label_list = label_list[smaller_box_idx]
 
             np_image = draw_labels(np_image, label_list.numpy(), label2str)
 
             if show_gui:
                 cv2.imshow("test", np_image)
                 cv2.waitKey(500)
+
+            if smaller_box_idx.sum() > 0:
+                print(
+                    f"Smaller than {minimum_pixel}x{minimum_pixel} box size detected! Total {smaller_box_idx.sum()} boxes."
+                )
+                # import pdb; pdb.set_trace()
 
 
 def test_augmentation(show_gui: bool = False):
--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -60,7 +60,7 @@ def test_load_images_and_labels(show_gui: bool = False):
         preprocess=lambda x: (x / 255.0).astype(np.float32),
         rect=False,
         pad=0,
-        mosaic_prob=1.0,
+        label_type="segments",
     )
 
     dataset_loader = DataLoader(

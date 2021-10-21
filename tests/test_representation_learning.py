96d000ca90d46b5effd69e5a835b92ca49b19676
Hyunwook Kim <32764235+wooks527@users.noreply.github.com>
2021-10-21 11:13:52 +0900

    Add base representation learning with L1 loss. (#13)
    * Add base representation learning with L1 loss.
    
    * Convert mkdir to makedirs
    
    * Fix typo
    
    * Increase n-skip and reduce image size for unittest
    
    * Reduce image size for unittest
    
    * Reduce image size for unittest
    
    Co-authored-by: Hyunwook Kim <hwkim@jmarple.ai>

--- tests/test_representation_learning.py
+++ tests/test_representation_learning.py
@@ -4,14 +4,25 @@
 - Contact: hwkim@jmarple.ai
 """
 
+import multiprocessing
 import gc
 import os
 import shutil
 from glob import glob
 
 import cv2
+import numpy as np
+import torch
+import yaml
+from kindle import Model
+from torch.utils.data import DataLoader
 
+from scripts.augmentation.augmentation import (AugmentationPolicy,
+                                               MultiAugmentationPolicies)
+from scripts.data_loader.data_loader_rl import LoadImagesForRL
 from scripts.representation_learning.crop_bboxes import crop_and_save_bboxes
+from scripts.train.yolo_rl_trainer import YoloRepresentationLearningTrainer
+from scripts.utils.torch_utils import select_device
 
 
 def test_crop_bboxes(show_gui: bool = False):
@@ -59,5 +70,82 @@ def test_crop_bboxes(show_gui: bool = False):
     assert num_cropped_imgs == num_targets
 
 
+def test_train_rl() -> None:
+    with open(
+        os.path.join("tests", "res", "configs", "train_config_rl.yaml"), "r"
+    ) as f:
+        cfg = yaml.safe_load(f)
+
+    cfg["train"]["epochs"] = 1
+    cfg["train"]["n_skip"] = 10
+    cfg["train"]["image_size"] = 320
+    if not torch.cuda.is_available():
+        cfg["train"]["device"] = "cpu"
+
+    device = select_device(cfg["train"]["device"], cfg["train"]["batch_size"])
+
+    aug_policy = AugmentationPolicy(cfg["augmentation"])
+
+    train_dataset = LoadImagesForRL(
+        "tests/res/datasets/coco/images/train2017",
+        img_size=cfg["train"]["image_size"],
+        batch_size=cfg["train"]["batch_size"],
+        n_skip=cfg["val"]["n_skip"],
+        augmentation=aug_policy,
+        preprocess=lambda x: (x / 255.0).astype(np.float32),
+        representation_learning=True,
+        n_trans=2,
+    )
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=cfg["train"]["batch_size"],
+        num_workers=multiprocessing.cpu_count() - 1,
+        collate_fn=LoadImagesForRL.collate_fn,
+    )
+    val_dataset = LoadImagesForRL(
+        "tests/res/datasets/coco/images/val2017",
+        img_size=cfg["train"]["image_size"],
+        batch_size=cfg["train"]["batch_size"],
+        n_skip=cfg["val"]["n_skip"],
+        augmentation=aug_policy,
+        preprocess=lambda x: (x / 255.0).astype(np.float32),
+        representation_learning=True,
+        n_trans=2,
+    )
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=cfg["train"]["batch_size"],
+        num_workers=multiprocessing.cpu_count() - 1,
+        collate_fn=LoadImagesForRL.collate_fn,
+    )
+
+    model = Model(
+        os.path.join("tests", "res", "configs", "model_yolov5s_rl.yaml"), verbose=True,
+    )
+
+    trainer = YoloRepresentationLearningTrainer(
+        model,
+        cfg,
+        train_dataloader=train_loader,
+        val_dataloader=val_loader,
+        device=device,
+        n_trans=2,
+    )
+    trainer.train()
+
+    del (
+        device,
+        aug_policy,
+        train_dataset,
+        train_loader,
+        val_dataset,
+        val_loader,
+        model,
+        trainer,
+    )
+    gc.collect()
+
+
 if __name__ == "__main__":
     test_crop_bboxes()
+    test_train_rl()

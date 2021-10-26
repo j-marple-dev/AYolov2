fcac3b1486fbfa4f321369519be01d4df8770ae2
Hyunwook Kim <hwkim@jmarple.ai>
2021-10-26 18:05:13 +0900

    Add unittest codes

--- tests/test_augmentation.py
+++ tests/test_augmentation.py
@@ -15,8 +15,9 @@ import yaml
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from scripts.augmentation.augmentation import (AugmentationPolicy,
-                                               MultiAugmentationPolicies)
+from scripts.augmentation.augmentation import (
+    AugmentationPolicy, MultiAugmentationPolicies,
+    MultiAugPoliciesWithUniformAugment)
 from scripts.data_loader.data_loader import LoadImages, LoadImagesAndLabels
 from scripts.utils.constants import LABELS
 from scripts.utils.plot_utils import draw_labels
@@ -139,6 +140,71 @@ def test_augmentation(show_gui: bool = False, p: float = 0.5):
     gc.collect()
 
 
+def test_uniform_augment(show_gui: bool = False, p: float = 0.5):
+    if random.random() > p:
+        return
+
+    label2str = LABELS["COCO"]
+    batch_size = 8
+    minimum_pixel = 4
+
+    with open(os.path.join("tests", "res", "configs", "train_config_uniaug.yaml")) as f:
+        cfg = yaml.safe_load(f)
+
+    aug_policy = MultiAugPoliciesWithUniformAugment(
+        cfg["augmentation"], cfg["uniform_augment"]
+    )
+
+    dataset = LoadImagesAndLabels(
+        "tests/res/datasets/coco/images/val2017",
+        cache_images=None,
+        n_skip=5,
+        img_size=320,
+        batch_size=batch_size,
+        rect=False,
+        augmentation=aug_policy,
+        yolo_augmentation=cfg["yolo_augmentation"],
+        label_type="segments",
+    )
+    dataset_loader = DataLoader(
+        dataset, batch_size=batch_size, collate_fn=LoadImagesAndLabels.collate_fn
+    )
+
+    pbar = tqdm(dataset_loader, desc="Load image test.")
+    n_run = 0
+    for (img, labels, _, _) in pbar:
+        n_run += 1
+        minimum_box_size = minimum_pixel / math.sqrt(img.shape[2] * img.shape[3])
+
+        for i in range(img.shape[0]):
+            np_image = img[i].numpy()[::-1].transpose((1, 2, 0))
+            label_list = labels[labels[:, 0] == i][:, 1:]
+
+            smaller_width_than_minimum_pixel = label_list[:, 3] < minimum_box_size
+            smaller_height_than_minimum_pixel = label_list[:, 4] < minimum_box_size
+
+            smaller_box_idx = torch.logical_or(
+                smaller_width_than_minimum_pixel, smaller_height_than_minimum_pixel
+            )
+
+            np_image = draw_labels(np_image, label_list.numpy(), label2str)
+
+            if show_gui:
+                cv2.imshow("test", np_image)
+                cv2.waitKey(0)
+
+            if smaller_box_idx.sum() > 0:
+                print(
+                    f"Smaller than {minimum_pixel}x{minimum_pixel} box size detected! Total {smaller_box_idx.sum()} boxes."
+                )
+
+            del np_image
+        gc.collect()
+    del dataset, dataset_loader
+    gc.collect()
+
+
 if __name__ == "__main__":
     test_augmentation(show_gui=True)
     # test_multi_aug_policies(show_gui=False)
+    test_uniform_augment(show_gui=False)

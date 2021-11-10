fc1a6a47ff1ef818dc1f709892cae7de0bce5046
Hyungseok Shin <hsshin@jmarple.ai>
2021-11-10 21:03:33 +0900

    Modify to evaluate AIGC formatted json

--- evaluate_json.py
+++ evaluate_json.py
@@ -1,5 +1,6 @@
 """Simple evaluate script for json result file."""
 import argparse
+import json
 import os
 
 from pycocotools.coco import COCO
@@ -31,13 +32,22 @@ if __name__ == "__main__":
     args = get_args()
     gt_path = os.path.join("tests", "res", "instances_val2017.json")
 
-    coco_eval = COCOmAPEvaluator(gt_path,)
-    result = coco_eval.evaluate(args.json_path)
+    coco_eval = COCOmAPEvaluator(gt_path)
+
+    json_path = args.json_path
+    with open(args.json_path, "r") as f:
+        result_json = json.load(f)
+    if "framework" in result_json[0].keys():
+        result_json = result_json[2:]
+        json_path = json_path.rsplit(".", 1)[0] + "_modified.json"
+        with open(json_path, "w") as f:
+            json.dump(result_json, f)
+    result = coco_eval.evaluate(json_path)
     LOGGER.info(f"mAP50: {result['map50']}, mAP50:95: {result['map50_95']}")
 
     if not args.no_coco:
         anno = COCO(gt_path)
-        pred = anno.loadRes(args.json_path)
+        pred = anno.loadRes(json_path)
         cocotools_eval = COCOeval(anno, pred, "bbox")
 
         cocotools_eval.evaluate()

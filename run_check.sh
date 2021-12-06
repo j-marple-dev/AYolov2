a1396e7fa5d494abd6b336c22fef0ff71904f2e0
Jongkuk Lim <lim.jeikei@gmail.com>
2021-12-06 10:54:28 +0900

    Update run_check.sh script for bash 3 support.

--- run_check.sh
+++ run_check.sh
@@ -5,46 +5,69 @@
 # - Author: Jongkuk Lim
 # - Contact: limjk@jmarple.ai
 
-declare -A CMD_DESC
+# Bash 3 does not support hash dictionary.
+# hput and hget are alternative workaround.
+# Usage)
+# hput $VAR_NAME $KEY $VALUE
+hput() {
+    eval "$1""$2"='$3'
+}
 
-CMD_DESC[format]="Run formating"
-CMD_DESC[lint]="Run linting check"
-CMD_DESC[test]="Run unit test"
-CMD_DESC[doc]="Generate MKDocs document"
-CMD_DESC[doc-server]="Run MKDocs hosting server (in local)"
-CMD_DESC[init-conda]="Create conda environment with default name"
-CMD_DESC[init-precommit]="Install pre-commit plugin"
-CMD_DESC[init]="Run init-conda and init-precommit"
-CMD_DESC[all]="Run formating, linting and unit test"
+# Usage)
+# `hget $VAR_NAME $KEY`
+hget() {
+    eval echo '${'"$1$2"'#hash}'
+}
 
-declare -A CMD_LIST
+# Define command names
+CMD_NAME=(
+    "format"
+    "lint"
+    "test"
+    "doc"
+    "doc-server"
+    "init-conda"
+    "init-precommit"
+    "init"
+    "all"
+    )
 
-CMD_LIST[format]="black . && \
-                  isort . && \
-                  docformatter -i -r . --wrap-summaries 88 --wrap-descriptions 88"
-CMD_LIST[lint]="env PYTHONPATH=. pytest --pylint --mypy --flake8 --ignore tests --ignore cpp"
-CMD_LIST[test]="env PYTHONPATH=. pytest tests --cov=scripts --cov-report term-missing --cov-report html"
-CMD_LIST[doc]="env PYTHONPATH=. mkdocs build --no-directory-urls"
-CMD_LIST[doc-server]="env PYTHONPATH=. mkdocs serve -a 127.0.0.1:8000 --no-livereload"
-CMD_LIST[init-conda]="conda env create -f environment.yml"
-CMD_LIST[init-precommit]="pre-commit install --hook-type pre-commit --hook-type pre-push"
-CMD_LIST[init]="${CMD_LIST[init-conda]} && ${CMD_LIST[init-precommit]}"
-CMD_LIST[all]="${CMD_LIST[format]} && ${CMD_LIST[lint]} && ${CMD_LIST[test]}"
+# Define descriptions
+hput CMD_DESC format "Run formating"
+hput CMD_DESC lint "Run linting check"
+hput CMD_DESC test "Run unit test"
+hput CMD_DESC doc "Generate MKDocs document"
+hput CMD_DESC doc_server "Run MKDocs hosting server (in local)"
+hput CMD_DESC init_conda "Create conda environment with default name"
+hput CMD_DESC init_precommit "Install pre-commit plugin"
+hput CMD_DESC init "Run init-conda and init-precommit"
+hput CMD_DESC all "Run formating, linting and unit test"
 
-exitCode=0
+# Define commands
+hput CMD_LIST format "black . && \ isort . && \ docformatter -i -r . --wrap-summaries 88 --wrap-descriptions 88"
+hput CMD_LIST lint "env PYTHONPATH=. pytest --pylint --mypy --flake8 --ignore tests --ignore cpp"
+hput CMD_LIST test "env PYTHONPATH=. pytest tests --cov=scripts --cov-report term-missing --cov-report html"
+hput CMD_LIST doc "env PYTHONPATH=. mkdocs build --no-directory-urls"
+hput CMD_LIST doc_server "env PYTHONPATH=. mkdocs serve -a 127.0.0.1:8000 --no-livereload"
+hput CMD_LIST init_conda "conda env create -f environment.yml"
+hput CMD_LIST init_precommit "pre-commit install --hook-type pre-commit --hook-type pre-push"
+hput CMD_LIST init "`hget CMD_LIST init_conda` && `hget CMD_LIST init_precommit`"
+hput CMD_LIST all "`hget CMD_LIST format` && `hget CMD_LIST lint` && `hget CMD_LIST test`"
+
 for _arg in $@
 do
-    if [[ ${CMD_LIST[$_arg]} == "" ]]; then
+    if [[ `hget CMD_LIST $_arg` == "" ]]; then
         echo "$_arg is not valid option!"
         echo "--------------- $0 Usage ---------------"
-        for _key in "${!CMD_LIST[@]}"
+        for _key in ${CMD_NAME[@]}
         do
-            echo "$0 $_key - ${CMD_DESC[$_key]}"
+            echo "$0 $_key - `hget CMD_DESC $_key`"
         done
         exit 0
     else
-        echo "Run ${CMD_LIST[$_arg]}"
-        eval ${CMD_LIST[$_arg]}
+        cmd=`hget CMD_LIST $_arg`
+        echo "Run $cmd"
+        eval $cmd
 
         result=$?
         if [ $result -ne 0 ]; then
